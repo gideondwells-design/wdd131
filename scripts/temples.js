3141af8d23a39dcfee034065c5fd9ee693635e01
Gideon David Wells <gideondwells@gmail.com>
2026-09-12 21:32:32 -0400

    Refactor script loading in HTML to use defer and remove unused event listener code

--- scripts/temples.js
+++ scripts/temples.js
@@ -5,12 +5,4 @@ if (document.getElementById("currentyear")) {
 if (document.getElementById("lastupdated")) {
 	document.getElementById("lastupdated").textContent = document.lastModified;
 }
-defer(function() {
-    const templeImages = document.querySelectorAll('img[alt^="Temple"]');
-    templeImages.forEach((img) => {
-        img.addEventListener('click', () => {
-            const templeNumber = img.alt.split(' ')[1];
-            alert(`You clicked on Temple ${templeNumber}`);
-        });
-    });
-})
+
