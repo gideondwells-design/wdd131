1295be586c004329eb51fba7ed7b56ac89147900
Gideon David Wells <gideondwells@gmail.com>
2026-09-12 21:19:46 -0400

    Add event listener for temple images and update CSS for navigation display

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,6 +1,16 @@
+
 if (document.getElementById("currentyear")) {
 	document.getElementById("currentyear").textContent = new Date().getFullYear();
 }
 if (document.getElementById("lastupdated")) {
 	document.getElementById("lastupdated").textContent = document.lastModified;
 }
+defer(function() {
+    const templeImages = document.querySelectorAll('img[alt^="Temple"]');
+    templeImages.forEach((img) => {
+        img.addEventListener('click', () => {
+            const templeNumber = img.alt.split(' ')[1];
+            alert(`You clicked on Temple ${templeNumber}`);
+        });
+    });
+})
